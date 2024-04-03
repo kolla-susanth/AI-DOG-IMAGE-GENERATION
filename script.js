22bebb0166dbd693be5ce03f5901b79de2c6fa6f
kolla-susanth <susanth3130@gmail.com>
2024-04-03 20:12:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const token = "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";
+const inptxt = document.getElementById("input");
+const image = document.getElementById("image");
+const button = document.getElementById("dtn");
+
+async function query(data) {
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/susanth73825/myepxwithai", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            method: "POST",
+            body: JSON.stringify(data),
+        }
+    );
+    const result = await response.blob();
+    return result;
+}
+button.addEventListener('click', async function() {
+    const inputValue = inptxt.value;
+    query({"inputs": inputValue}).then((response) => {
+        const objectURL = URL.createObjectURL(response);
+        image.src = objectURL;
+    });
+});
